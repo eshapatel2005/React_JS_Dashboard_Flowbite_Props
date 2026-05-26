af6fa20774c0979e1c1c638894c73d8d42d54b42
eshapatel2005 <esha.patel5605@gmail.com>
2026-05-26 16:08:22 +0530

    suseState with user object

--- src/components/State_Component.jsx
+++ src/components/State_Component.jsx
@@ -2,14 +2,32 @@ import React from 'react'
 import {useState} from 'react'
 
 function State_Component() {
-    const [count,setCount]=useState(0)
+    
+    //useState with counter
+    //const [count,setCount]=useState(0)
+    
+    //useState with user object
+    const [user,setUser]=useState({
+        name:"esha",
+        age:21,
+        city:"surat"
+    });
+    const updateUser=()=>{
+        setUser({
+            ...user,
+            name:"john",
+            age:25,
+            city:"mumbai"
+        })
+    }
+    console.log(user)
 
   return (
     <>
-    <div>State_Component</div>
+    {/*<div>State_Component</div>
     
     <button className="btn1" onClick={()=>setCount(count+1)}>Increase</button>
-    <h1>Count : {count}</h1>
+    <h1>Count : {count}</h1>*/}
     </>
   )
 }
