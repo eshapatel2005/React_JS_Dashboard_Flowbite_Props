533e7408f1a964bbe2e5374e5572770a8800a1ec
eshapatel2005 <esha.patel5605@gmail.com>
2026-05-27 15:31:40 +0530

    Event handling with inline function

--- src/components/Events_Component.jsx
+++ src/components/Events_Component.jsx
@@ -6,11 +6,21 @@ function Event_Component() {
     function handleClick(){
         alert("Hello, you clicked the button!")
     }
+
+    //Event handling with inline function
+    const greetUser = (name) => {  
+        alert(`Hello ${name}`);   
+    }
   return (
     <>
         <div>events_component</div>
         <button className="btn1" onClick={handleClick}>Click Me</button>
+
+        {/* Event handling with inline function */}
         <button className="btn1" onClick={()=>alert("esha")}>CLICK</button>
+
+        {/* Event handling with inline function */}
+        <button className="btn1" onClick={() => greetUser("ESHA")}>Greet</button> 
     </>
   )
 }
