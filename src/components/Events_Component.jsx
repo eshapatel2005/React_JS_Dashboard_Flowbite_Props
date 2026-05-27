dd3ac1e0c14c7208dd13aa93d613cb0018e8743f
eshapatel2005 <esha.patel5605@gmail.com>
2026-05-27 16:04:08 +0530

    Toggle Light/Dark Mode

--- src/components/Events_Component.jsx
+++ src/components/Events_Component.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {useState} from 'react'
 
 function Event_Component() {
     
@@ -16,13 +17,16 @@ function Event_Component() {
     const handleEvent = (event) => { 
         console.log(event); 
     }
+
+    //Toggle Light/Dark Mode
+    const [mode,setMode]=useState("light");
         
   return (
     <>
         <div>events_component</div>
 
         {/* Event handling with function */}
-        <button className="btn1" onClick={handleClick}>Click Me</button>
+        <button className="btn1" onMouseOver={handleClick}>Click Me</button>
 
         {/* Event handling with inline function */}
         <button className="btn1" onClick={()=>alert("esha")}>CLICK</button>
@@ -31,7 +35,10 @@ function Event_Component() {
         <button className="btn1" onClick={() => greetUser("ESHA")}>Greet</button> 
 
         {/* Event handling with inline function and passing event object */}
-        <button className="btn1" onClick={handleEvent}>Click It</button>
+        <button className="btn1" onDoubleClick={handleEvent}>Click It</button>
+
+        {/* Toggle Light/Dark Mode */}
+        <button className='btn2' onClick={()=>setMode(mode==="light"?"dark":"light")}>Toggle Mode:{mode}</button>
     </>
   )
 }
