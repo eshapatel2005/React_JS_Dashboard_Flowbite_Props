be93ef94444063cf77bf13814724811520c02f82
eshapatel2005 <esha.patel5605@gmail.com>
2026-05-29 16:29:29 +0530

    Add new data to array uisng button click and display it using map

--- src/components/MapComponents.jsx
+++ src/components/MapComponents.jsx
@@ -1,22 +1,27 @@
 import React from "react";
+import { useState } from "react";
 
 function MapComponents() {
   // Mapping an array of objects to JSX elements
   const numbers = [
     { id: 1, name: "Esha", age: 21 },
     { id: 2, name: "Avi", age: 15 },
-  ]
+  ];
 
   // Display products in a card-like format using map
   const products = [
     { id: 1, name: "iPhone 15", price: 75000 },
     { id: 2, name: "Samsung Galaxy S24", price: 70000 },
-  ]
+  ];
 
   // Mapping an array of objects with nested arrays to JSX elements
-  const data = [
-    { id: 1, name: "ESHA", hobbies: ["Singing", "Traveling"] }
-  ]
+  const data = [{ id: 1, name: "ESHA", hobbies: ["Singing", "Traveling"] }];
+
+  //Add new data to array uisng button click and display it using map
+  const [users, setUsers] = useState(["Esha", "Raj"]);
+  function addUser() {
+    setUsers([...users, "Neha"]);
+  }
 
   return (
     <>
@@ -57,6 +62,15 @@ function MapComponents() {
             ))}{" "}
           </div>
         ))}{" "}
+      </div>
+
+      {/* Add new data to array using button click and display it using map */}
+      <div>
+        <button onClick={addUser}>Add User</button>
+
+        {users.map((user, index) => (
+          <h2 key={index}>{user}</h2>
+        ))}
       </div>
     </>
   );
